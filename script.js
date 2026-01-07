de3ed54fd5b3c0b0617a63d854b014acbad48d4a
Afsana-anu <afsananazahathmd9@gmail.com>
2026-01-07 17:20:12 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,20 @@
+fetch('servers.json')
+  .then(response => response.json())
+  .then(data => {
+    const search = document.getElementById('search');
+    const result = document.getElementById('result');
+
+    search.addEventListener('keyup', () => {
+      const value = search.value.toLowerCase();
+      const server = data.find(s => s.name === value);
+
+      if (server) {
+        result.innerHTML = `<p>
+          ✅ Server Found:<br>
+          <a href="${server.url}" target="_blank">${server.url}</a>
+        </p>`;
+      } else {
+        result.innerHTML = `<p>❌ Server not found</p>`;
+      }
+    });
+  });
